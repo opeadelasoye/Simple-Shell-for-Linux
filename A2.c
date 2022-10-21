7336da69ac1d2f47b227a5a3914e656f88639a36
Ope Adelasoye <on861917@dal.ca>
2022-10-20 23:24:50 -0300

    Updating history feature

--- A2.c
+++ A2.c
@@ -5,21 +5,34 @@
 #include <sys/wait.h>
 
 #define MAX_LINE 80
+#define MAX_HISTORY 10
 
-int main(void){
+int main(){
 	char *args[MAX_LINE/2 + 1];
 	char cmdLineInput[MAX_LINE];
+	int execvp_status;
 	int wait_status;
 	int should_run = 1;
 	int i;
 	
+	int historyTracker = 0;
+	int historyID[MAX_HISTORY];
+	int historyPID[MAX_HISTORY];
+	char historyCommand[10][MAX_LINE];
+	
 	while(should_run == 1){
-		memset(args, 0, MAX_LINE/2 + 1);
+		memset(args, 0, sizeof(args));
+		memset(cmdLineInput, 0, sizeof(cmdLineInput));
 		i = 0;
 		printf("CSCI3120>");
 		fflush(stdout);
 
 		fgets(cmdLineInput, MAX_LINE, stdin);
+		char currentInput[MAX_LINE];
+		int x;
+		for(x = 0; x < sizeof(cmdLineInput); x++){
+			currentInput[x] = cmdLineInput[x];
+		}
 		char *splitInput = strtok(cmdLineInput, " ");
 		while(splitInput != NULL){
 			args[i] = splitInput;
@@ -30,20 +43,50 @@ int main(void){
 			args[i-1][strlen(args[i-1])-1]= '\0'; //remove new line
 		}
 		args[i] = NULL;
-		if(strcmp(args[0], "exit") == 0){
-				should_run = 0;
-				break;
-		}
+		
+		currentInput[strlen(currentInput)-1]= '\0';
 		
 		pid_t pid;
 		pid = fork(); //fork a child process
+		
+		if(strcmp(args[0], "exit") == 0){
+			should_run = 0;
+			break;
+		}else if(strcmp(args[0], "history") == 0){
+			int j;
+			if(historyTracker > 0){
+				printf("ID\tPID\tCommand\n");
+				for(j = 0; j < historyTracker; j++){
+					printf("%d\t%d\t%s\n", historyID[j], historyPID[j], historyCommand[j]);
+				}
+			}else{
+				printf("No commands recorded yet\n");
+			}
+		}else{			
+			if(historyTracker < 10){
+				historyID[historyTracker] = historyTracker+1;
+				historyPID[historyTracker] = pid;
+				strcpy(historyCommand[historyTracker], currentInput);
+				historyTracker++;
+			}else{
+				int l;
+				for(l = 0; l < 9; l++){
+					historyID[l] = historyID[l + 1];
+					historyPID[l] = historyPID[l + 1];
+					strcpy(historyCommand[l], historyCommand[l+1]);
+				}
+				historyID[l] = 10;
+				historyPID[l] = pid;
+				strcpy(historyCommand[l], currentInput);				
+			}
+		}
 		
 		if(pid < 0){
 			printf("Error: Process failed\n");
 			return 1;
 		}else if(pid == 0){ //child process
-			int execvp_status = execvp(args[0], args);
-			if(execvp_status == -1){
+			execvp_status = execvp(args[0], args);
+			if(execvp_status == -1 && ((strcmp(args[0], "history") > 0) || (strcmp(args[0], "history") < 0))){
 				printf("Error: Invalid command\n");
 				exit(1);
 			}
@@ -51,5 +94,5 @@ int main(void){
 			wait(&wait_status);
 		}
 	}
-	return 0;
+	exit(0);
 }
