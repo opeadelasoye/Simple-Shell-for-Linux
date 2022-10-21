d623b5180d266695ca0081d81111a8ada431d772
Ope Adelasoye <on861917@dal.ca>
2022-10-21 15:20:14 -0300

    Added history features (!! and !N)

--- A2.c
+++ A2.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 #define MAX_LINE 80
 #define MAX_HISTORY 10
@@ -10,13 +11,11 @@
 int main(){
 	char *args[MAX_LINE/2 + 1];
 	char cmdLineInput[MAX_LINE];
-	int execvp_status;
-	int wait_status;
+	int execvp_status, wait_status;
 	int should_run = 1;
 	int i;
 	
 	int historyTracker = 0;
-	int historyCalled;
 	int historyID[MAX_HISTORY];
 	int historyPID[MAX_HISTORY];
 	char historyCommand[MAX_HISTORY][MAX_LINE];
@@ -24,6 +23,7 @@ int main(){
 	while(should_run == 1){
 		memset(args, 0, sizeof(args));
 		memset(cmdLineInput, 0, sizeof(cmdLineInput));
+		int historyCalled = 1, exitCalled = 1, recentCalled = 1;
 		i = 0;
 		printf("CSCI3120>");
 		fflush(stdout);
@@ -49,7 +49,7 @@ int main(){
 		
 		if(strcmp(args[0], "exit") == 0){
 			should_run = 0;
-			break;
+			exitCalled = 0;
 		}else if(strcmp(args[0], "history") == 0){
 			historyCalled = 0;
 			int j;
@@ -59,14 +59,52 @@ int main(){
 					printf("%d\t%d\t%s\n", historyID[j], historyPID[j], historyCommand[j]);
 				}
 			}else{
-				printf("No commands recorded yet\n");
+				printf("No command in history!\n");
 			}
-		}else{
-			historyCalled = 1;
+		}else if(strcmp(args[0], "!!") == 0){
+			recentCalled = 0;
+			if(historyTracker == 0){
+				printf("No command in history!\n");
+				historyCalled = 0;
+			}else{
+				printf("%s\n", historyCommand[historyTracker - 1]);
+				i = 0;
+				strcpy(cmdLineInput, historyCommand[historyTracker - 1]);
+				char *recentInput = strtok(cmdLineInput, " ");
+				while(recentInput != NULL){
+					args[i] = recentInput;
+					recentInput = strtok(NULL, " \n");
+					i++;
+				}
+				args[i] = NULL;	
+			}
+		}else if(isdigit(args[0][1]) > 0){
+			recentCalled = 0;
+			if(args[0][1] - 48 <= 0 || args[0][1] - 48 > 10){
+				printf("Such a command is not in history 1");	
+			}else{
+				if(historyID[args[0][1]] - 48 > historyTracker){
+					printf("Such a command is not in history 2");
+				}else{
+					printf("%s\n", historyCommand[args[0][1] - 48 - 1]);
+					i = 0;
+					strcpy(cmdLineInput, historyCommand[args[0][1] - 48 - 1]);
+					char *recentInput = strtok(cmdLineInput, " ");
+					while(recentInput != NULL){
+						args[i] = recentInput;
+						recentInput = strtok(NULL, " \n");
+						i++;
+					}
+					args[i] = NULL;	
+				}
+			}
+			
 		}
 		
 		pid_t pid;
-		pid = fork(); //fork a child process
+		if(historyCalled == 1 && exitCalled == 1){
+			pid = fork();
+		}
 		
 		if(historyTracker < 10 && historyCalled == 1){
 			historyID[historyTracker] = historyTracker+1;
@@ -81,16 +119,19 @@ int main(){
 			}
 			historyID[l] = 10;
 			historyPID[l] = pid;
-			strcpy(historyCommand[l], currentInput);				
-		}		
+			strcpy(historyCommand[l], currentInput);			
+		}	
 		
 		if(pid < 0){
-			printf("Error: Process failed\n");
-			return 1;
+			if(historyCalled == 0 || exitCalled == 0 || recentCalled == 0){
+			}else{
+				printf("Error: Process failed\n");
+				return 1;
+			}
 		}else if(pid == 0){ //child process
 			execvp_status = execvp(args[0], args);
-			if(execvp_status == -1 && ((strcmp(args[0], "history") > 0) || (strcmp(args[0], "history") < 0))){
-				printf("Error: Invalid command\n");
+			if(execvp_status == -1 && ((strcmp(args[0], "history") < 0))){
+				printf("Invalid command!\n");
 				exit(1);
 			}
 		}else{ //parent process
